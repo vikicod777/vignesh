13c6c5c2bf1b035e0ebaf8e51e6525e90a28dcb3
vikicod777 <68887833+vikicod777@users.noreply.github.com>
2020-08-02 01:21:10 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+  
+package com.helloworld;
+
+public class HelloWorl {
+   private String message;
+
+   public void setMessage(String message){
+      this.message  = message;
+   }
+   public void getMessage(){
+      System.out.println("Your Message : " + message);
+   }
+}
